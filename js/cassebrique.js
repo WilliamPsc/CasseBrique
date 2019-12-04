92647031d3275e5cf634fbf523acd11145205565
William Psc <pensec.william@gmail.com>
2019-12-04 17:19:58 +0100

    1.7.2 - feat
    Test to merge

--- js/cassebrique.js
+++ js/cassebrique.js
@@ -1,7 +1,6 @@
 /**
  * @author Timothé LANNUZEL et William PENSEC
- * @version 1.0
- * @date 27/11/2019
+ * @version 1.6
  * @description Script servant à faire tourner le jeu du casse brique
  * 
 ** /
@@ -11,6 +10,27 @@ var canvas = document.getElementById("myCanvas");
 canvas.style.backgroundColor = "black"; // Fond en noir
 var nbBriqueRestante = -1;
 var ctx = canvas.getContext("2d");
+var beg = false;
+
+/* Création sprite audio */
+var musiqueActive = false;
+var sonActif = false;
+
+var musiqueFond = document.createElement("audio");
+musiqueFond.src = "musique/background.mp3";
+musiqueFond.volume = 0.7;
+
+var bruitBrique = document.createElement("audio");
+bruitBrique.src = "musique/bruit_brique.mp3";
+bruitBrique.volume = 0.5;
+
+var bruitRebond = document.createElement("audio");
+bruitRebond.src = "musique/rebond.mp3";
+bruitRebond.volume = 0.4;
+
+/*var bruitBonus = document.createElement("audio");
+bruitBonus.src = "musique/bonus.mp3";
+bruitBonus.volume = 0.5;*/
 
 /* Initialisation position balle */
 var diam = 20;
@@ -24,45 +44,167 @@ var longBarre = 200;
 var hautBarre = 20;
 var posBarreX = (canvas.width / 2) - (longBarre / 1.9);
 var posBarreY = canvas.height - (hautBarre * 2);
+var modeAutomatique = false;
+
 
 /* Initialisation briques */
-var nbLigne = 10;
-var nbColomne = 10;
-var espace_brique = 6;
-var marge_gauche_brique = 15;
-var marge_haut_brique = 10;
-var largeur_brique = (canvas.width) / (nbColomne + (espace_brique * nbColomne)/150);
-var hauteur_brique = (canvas.height) / (nbLigne * 4);
+var nbLigne = 5;
+var nbColonne = 10;
+var espace_brique = 10;
+var marge_gauche_brique = 7;
+var marge_haut_brique = 25;
+var largeur_brique = ((canvas.width) / (nbColonne)) - espace_brique;
+var hauteur_brique = 40;
 var tableauBrique = [];
-for (var i = 0; i < nbColomne; i++) {
+for (var i = 0; i < nbColonne; i++) {
     tableauBrique[i] = [];
     for (var j = 0; j < nbLigne; j++) {
         tableauBrique[i][j] = 1;
     }
 }
 
+/* Variables chronometre */
+var start = 0;
+var end = 0;
+var diff = 0;
+
 /* Vérification si on a gagné */
 var score = 0;
-nbBriqueRestante = nbColomne * nbLigne;
+var vie = -1;
+nbBriqueRestante = nbColonne * nbLigne;
 
 /* Mouvement Souris*/
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
-/* Mouvement Tactil*/
+/* Mouvement Tactile */
 document.addEventListener("touchstart", touchHandler);
 document.addEventListener("touchmove", touchHandler);
 
-function pause() {
-    alert("Pause");
+function setColonne() {
+    localStorage.nbColonne = document.getElementById("nbColonne").value;
+    nbColonne = Number(localStorage.getItem("nbColonne"));
+    document.getElementById("labelSetColonne").innerHTML = "Valeur : " + nbColonne;
+
+    largeur_brique = ((canvas.width) / (nbColonne)) - espace_brique;
+    setLigne();
+}
+
+function setLigne() {
+    localStorage.nbLigne = document.getElementById("nbLigne").value;
+    nbLigne = Number(localStorage.getItem("nbLigne"));
+    document.getElementById("labelSetLigne").innerHTML = "Valeur : " + nbLigne;
+
+    for (var i = 0; i < nbColonne; i++) {
+        tableauBrique[i] = [];
+        for (var j = 0; j < nbLigne; j++) {
+            tableauBrique[i][j] = 1;
+        }
+    }
+    score = 0;
+    nbBriqueRestante = nbColonne * nbLigne;
+
+    posBalleX = canvas.width / 2;
+    posBalleY = canvas.height - 100;
+    moveX = 5;
+    moveY = -5;
+
+    drawBall();
+    drawBrique();
+}
+
+function setVies() {
+    localStorage.vie = document.getElementById("nbVies").value;
+    vie = Number(localStorage.getItem("vie"));
+    document.getElementById("labelSetVies").innerHTML = "Valeur : " + vie;
+}
+
+function setAuto() {
+    var valeur = "";
+    modeAutomatique = !modeAutomatique;
+    if (modeAutomatique == true) {
+        valeur = "On";
+        document.getElementById("Auto").value = true;
+    }
+    else {
+        valeur = "Off";
+        document.getElementById("Auto").value = false;
+    }
+    document.getElementById("labelModeAuto").innerHTML = valeur;
+}
+
+function setMusique() {
+    var valeur = "";
+    musiqueActive = !musiqueActive;
+    if (musiqueActive == true) {
+        valeur = "On";
+        document.getElementById("musique").value = true;
+    }
+    else {
+        valeur = "Off";
+        document.getElementById("musique").value = false;
+    }
+    document.getElementById("labelMusique").innerHTML = valeur;
+    musique();
+}
+
+function musique() {
+    if (musiqueActive == true) {
+        console.log("Musique de fond");
+        musiqueFond.play();
+        musiqueFond.onended = (event) => {
+            console.log("Ended");
+            musique();
+        }
+    }
+    else {
+        console.log("Musique de fond NON");
+        musiqueFond.pause();
+    }
+}
+
+function setSon() {
+    var valeur = "";
+    sonActif = !sonActif;
+    if (sonActif == true) {
+        valeur = "On";
+        document.getElementById("son").value = true;
+    }
+    else {
+        valeur = "Off";
+        document.getElementById("son").value = false;
+    }
+    document.getElementById("labelSon").innerHTML = valeur;
 }
 
 function win() {
     score = -1;
-    if (window.confirm("================ GAGNÉ ================ \n Cliquez sur OK pour rejouer \n Cliquez sur Annuler pour revenir à l'accueil")) {
+    end = new Date();
+    diff = end - start;
+    diff = new Date(diff);
+    var sec = diff.getSeconds();
+    var min = diff.getMinutes();
+    var heure = diff.getHours() - 1;
+
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    if (min == 0) {
+        min = "00";
+    } else if (min < 10) {
+        min = "0" + min;
+    }
+    if (heure == 0) {
+        heure = "00";
+    } else if (heure < 10) {
+        heure = "0" + heure;
+    }
+    ctx.fillText(heure + " : " + min + " : " + sec, canvas.width - 100, 20);
+    clearInterval(10); // Needed for Chrome to end game
+
+    if (window.confirm("================ GAGNÉ ================ \n Temps mis : " + heure + " : " + min + " : " + sec + "\n\n Cliquez sur OK pour rejouer \n Cliquez sur Annuler pour revenir à l'accueil")) {
         document.location.reload();
-        clearInterval(10); // Needed for Chrome to end game
     } else {
-        document.location.href = "/index.html";
+        document.location.href = "index.html";
     }
 }
 
@@ -86,7 +228,7 @@ function drawBarre() {
 }
 
 function drawBrique(){
-    for(var i = 0; i < nbColomne; i++){
+    for(var i = 0; i < nbColonne; i++){
         for (var j = 0; j < nbLigne; j++){
             if (tableauBrique[i][j] == 1) {
                 var briqueX = (i * (largeur_brique + espace_brique)) + (marge_gauche_brique);
@@ -105,14 +247,25 @@ function moveBall() {
     posBalleX += moveX;
     posBalleY += moveY;
 
+    if (modeAutomatique == true) {
+        posBarreX = posBalleX - (longBarre / 2);
+    }
     if (posBalleY + diam >= canvas.height) {
-        console.log("YOU DIE");
-        /*if (window.confirm("================ GAME OVER ================ \n Cliquez sur OK pour rejouer \n Cliquez sur Annuler pour revenir à l'accueil")) {
-            document.location.reload();
-            clearInterval(1); // Needed for Chrome to end game
-        } else {
-            document.location.href = "/index.html";
-        }*/
+        if (vie != -1) {
+            vie--;
+            if (vie < 0) {
+                console.log("YOU DIE");
+                if (window.confirm("================ GAME OVER ================ \n Cliquez sur OK pour rejouer \n Cliquez sur Annuler pour revenir à l'accueil")) {
+                    document.location.reload();
+                    clearInterval(1); // Needed for Chrome to end game
+                } else {
+                    document.location.href = "index.html";
+                }
+            }
+        }
+        else {
+            console.log("Mode infini");
+        }
     }
 
     if (posBalleX + moveX > canvas.width - diam || posBalleX + moveX < 0 + diam) {
@@ -135,47 +288,81 @@ function moveBall() {
 
 document.onkeydown = function (event) {
     switch (event.keyCode) {
-        case 37: //Gauche
+        case 37: // Gauche
             if (posBarreX > 0) {
-                posBarreX -= 40;
+                if (modeAutomatique != true) {
+                    posBarreX -= 60;
+                }
             }
             break;
-        case 39: //Droite
+        case 39: // Droite
             if (posBarreX + longBarre < canvas.width) {
-                posBarreX += 40;
+                if (modeAutomatique != true) {
+                    posBarreX += 60;
+                }
             }
             break;
-        case 80: //Touche p
-            pause();
+        case 80: // Touche p
+            beg = !beg;
+            break;
+        case 66: // Touche b
+            beg = !beg;
+            start = new Date();
             break;
     }
 }
 
 //Fonction controle souris
 function mouseMoveHandler(e) {
-    var positionSourisX = e.clientX - canvas.offsetLeft;
-    if (positionSourisX > 0 && positionSourisX < canvas.width) {
-        posBarreX = positionSourisX - longBarre / 2;
+    if (modeAutomatique != true) {
+        var rect = canvas.getBoundingClientRect();
+        var futurPosBarreX = (e.clientX - rect.left) * (canvas.width / rect.width) - (longBarre / 2)
+        if (futurPosBarreX < 0) {
+            posBarreX = 0;
+        } else {
+            if (futurPosBarreX > canvas.width - longBarre) {
+                posBarreX = canvas.width - longBarre;
+            } else {
+                posBarreX = futurPosBarreX;
+            }
+        }
     }
 }
 
 //Fonction controle tactile
 function touchHandler(e) {
-    if (e.touches) {
-        if (e.touches[0].clientX > 0 && e.touches[0].clientX < canvas.width) {
-            posBarreX = e.touches[0].clientX - longBarre / 2;
+    if (beg != true) {
+        beg = true;
+        start = new Date()
+    }
+    if (modeAutomatique != true) {
+        var rect = canvas.getBoundingClientRect();
+        if (e.touches) {
+            var futurPosBarreX = (e.touches[0].clientX - rect.left) * (canvas.width / rect.width) - (longBarre / 2)
+            if (futurPosBarreX < 0) {
+                posBarreX = 0;
+            } else {
+                if (futurPosBarreX > canvas.width - longBarre) {
+                    posBarreX = canvas.width - longBarre;
+                } else {
+                    posBarreX = futurPosBarreX;
+                }
+            }
         }
     }
 }
 
 function collision() {
-    for (var i = 0; i < nbColomne; i++) {
+    for (var i = 0; i < nbColonne; i++) {
         for (var j = 0; j < nbLigne; j++) {
             var briqueX = (i * (largeur_brique + espace_brique)) + (marge_gauche_brique);
             var briqueY = (j * (hauteur_brique + espace_brique)) + (marge_haut_brique);
             if (posBalleY <= briqueY + hauteur_brique + marge_haut_brique && posBalleY >= briqueY) {
                 if (posBalleX <= briqueX + largeur_brique + marge_gauche_brique && posBalleX >= briqueX) {
                     if (tableauBrique[i][j] == 1) {
+                        if (sonActif == true) {
+                            bruitBrique.play();
+                        }
                         posBalleY += moveY;
                         moveY = -moveY;
                         tableauBrique[i][j] = 0;
@@ -185,16 +372,19 @@ function collision() {
             }
         }
     }
-
 }
 
 function collisionBarre() {
     if ((posBalleX + moveX) > posBarreX && (posBalleX + moveX) < (posBarreX + longBarre)) {
         if ((posBalleY + moveY + diam) > posBarreY && (posBalleY + moveY + diam) < posBarreY + hautBarre) {
+            if (sonActif == true) {
+                bruitRebond.play();
+            }
             console.log("Rebond Barre");
             //Bouger la balle selon la raquette
             if ((posBalleX + moveX) < (posBarreX + (longBarre / 5))) {
                 console.log("Rebond Barre -3");
+
                 if (moveX >= -10) {
                     moveX = moveX - 3;
                 }
@@ -226,35 +416,69 @@ function collisionBarre() {
                             }
                             moveY = -moveY;
                         }
-
                     }
                 }
             }
-
         }
     }
+}
+
+function chronometre() {
+    end = new Date();
+    diff = end - start;
+    diff = new Date(diff);
+    var sec = diff.getSeconds();
+    var min = diff.getMinutes();
+    var heure = diff.getHours() - 1;
+
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+    if (min == 0) {
+        min = "00";
+    }else if (min < 10) {
+        min = "0" + min;
+    }
+    if (heure == 0) {
+        heure = "00";
+    } else if (heure < 10) {
+        heure = "0" + heure;
+    }
+
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillText(heure + " : " + min + " : " + sec, canvas.width - 100, 20);
 }
 
 function drawText() {
     ctx.font = "20px Arial";
     ctx.fillStyle = "#FFFFFF";
     ctx.fillText("Score: " + score, 10, 20);
-    ctx.fillText("x :" + posBalleX + " | y : " + posBalleY, canvas.width - 150, canvas.height - 10);
-    ctx.fillText("Vitesse balle x :" + moveX + " | Vitesse balle y : " + moveY, 10, canvas.height - 10);
+    ctx.fillText("Brique restante : " + (nbBriqueRestante - score), 925, 20);
+    ctx.fillText("x : " + posBalleX + " | y : " + posBalleY, canvas.width - 150, canvas.height - 10);
+    ctx.fillText("Vitesse balle x : " + Math.abs(moveX), 10, canvas.height - 10);
+    ctx.fillText("Vies restantes : " + vie, 925, canvas.height - 10);
 }
 
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawBarre();
-    drawBrique();
-    if (score == nbBriqueRestante) {
-        win();
-    }
-    moveBall();
-    collision();
+    if (beg == true) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+        drawBarre();
+        drawBrique();
+        if (score == nbBriqueRestante) {
+            setTimeout(win(), 1000);
+        }
+        moveBall();
+        collision();
 
-    drawText();
+        drawText();
+        chronometre();
+    } else {
+        ctx.font = "40px Arial";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fillText("APPUYEZ SUR LA TOUCHE \"B\" OU TOUCHER L'ÉCRAN POUR COMMENCER À JOUER !", 150, canvas.height / 2);
+    }
 }
 setInterval(draw, 10);
 requestAnimationFrame(draw);
